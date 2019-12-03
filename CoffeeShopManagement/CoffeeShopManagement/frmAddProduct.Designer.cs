323155eb8fde3114542c1670a0e65b6a73827bec
github-minhlong <mail.minhlong@gmail.com>
2019-12-03 16:59:58 +0700

    CoffeeShopManagement

---
+++ CoffeeShopManagement/CoffeeShopManagement/frmAddProduct.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CoffeeShopManagement
+{
+    partial class frmAddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(12, 18);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(48, 18);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSize.Location = new System.Drawing.Point(12, 57);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(37, 18);
+            this.lblSize.TabIndex = 1;
+            this.lblSize.Text = "Size";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(12, 93);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(42, 18);
+            this.lblPrice.TabIndex = 2;
+            this.lblPrice.Text = "Price";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(65, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(192, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(192, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(54, 135);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(158, 135);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmAddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 174);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmAddProduct";
+            this.Text = "frmAddProduct";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
--- CoffeeShopManagement/CoffeeShopManagement/frmDashboard.Designer.cs
+++ CoffeeShopManagement/CoffeeShopManagement/frmDashboard.Designer.cs
@@ -28,19 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addProductToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewAllProductToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewAllSalesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grbProducts = new System.Windows.Forms.GroupBox();
+            this.grdProducts = new System.Windows.Forms.DataGridView();
+            this.grbBill = new System.Windows.Forms.GroupBox();
+            this.grdBill = new System.Windows.Forms.DataGridView();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnCheckOut = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.grbProducts.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdProducts)).BeginInit();
+            this.grbBill.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdBill)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addProductToolStripMenuItem,
+            this.viewAllProductToolStripMenuItem,
+            this.viewAllSalesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(654, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addProductToolStripMenuItem
+            // 
+            this.addProductToolStripMenuItem.Name = "addProductToolStripMenuItem";
+            this.addProductToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.addProductToolStripMenuItem.Text = "Add product";
+            // 
+            // viewAllProductToolStripMenuItem
+            // 
+            this.viewAllProductToolStripMenuItem.Name = "viewAllProductToolStripMenuItem";
+            this.viewAllProductToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.viewAllProductToolStripMenuItem.Text = "View All Product";
+            // 
+            // viewAllSalesToolStripMenuItem
+            // 
+            this.viewAllSalesToolStripMenuItem.Name = "viewAllSalesToolStripMenuItem";
+            this.viewAllSalesToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.viewAllSalesToolStripMenuItem.Text = "View All Sales";
+            // 
+            // grbProducts
+            // 
+            this.grbProducts.Controls.Add(this.grdProducts);
+            this.grbProducts.Location = new System.Drawing.Point(12, 27);
+            this.grbProducts.Name = "grbProducts";
+            this.grbProducts.Size = new System.Drawing.Size(248, 278);
+            this.grbProducts.TabIndex = 1;
+            this.grbProducts.TabStop = false;
+            this.grbProducts.Text = "Products";
+            // 
+            // grdProducts
+            // 
+            this.grdProducts.AllowUserToAddRows = false;
+            this.grdProducts.AllowUserToDeleteRows = false;
+            this.grdProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdProducts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdProducts.Location = new System.Drawing.Point(3, 16);
+            this.grdProducts.Name = "grdProducts";
+            this.grdProducts.ReadOnly = true;
+            this.grdProducts.Size = new System.Drawing.Size(242, 259);
+            this.grdProducts.TabIndex = 0;
+            // 
+            // grbBill
+            // 
+            this.grbBill.Controls.Add(this.grdBill);
+            this.grbBill.Location = new System.Drawing.Point(311, 43);
+            this.grbBill.Name = "grbBill";
+            this.grbBill.Size = new System.Drawing.Size(296, 230);
+            this.grbBill.TabIndex = 2;
+            this.grbBill.TabStop = false;
+            this.grbBill.Text = "Bill";
+            // 
+            // grdBill
+            // 
+            this.grdBill.AllowUserToAddRows = false;
+            this.grdBill.AllowUserToDeleteRows = false;
+            this.grdBill.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdBill.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdBill.Location = new System.Drawing.Point(3, 16);
+            this.grdBill.Name = "grdBill";
+            this.grdBill.ReadOnly = true;
+            this.grdBill.Size = new System.Drawing.Size(290, 211);
+            this.grdBill.TabIndex = 0;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(310, 280);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(56, 24);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "Total";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(390, 284);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(115, 20);
+            this.txtTotal.TabIndex = 3;
+            // 
+            // btnCheckOut
+            // 
+            this.btnCheckOut.Location = new System.Drawing.Point(529, 282);
+            this.btnCheckOut.Name = "btnCheckOut";
+            this.btnCheckOut.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckOut.TabIndex = 4;
+            this.btnCheckOut.Text = "Check out";
+            this.btnCheckOut.UseVisualStyleBackColor = true;
             // 
             // frmDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(546, 317);
+            this.ClientSize = new System.Drawing.Size(654, 317);
+            this.Controls.Add(this.btnCheckOut);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.grbBill);
+            this.Controls.Add(this.grbProducts);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmDashboard";
             this.Text = "frmDashboard";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.grbProducts.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdProducts)).EndInit();
+            this.grbBill.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdBill)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addProductToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewAllProductToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewAllSalesToolStripMenuItem;
+        private System.Windows.Forms.GroupBox grbProducts;
+        private System.Windows.Forms.DataGridView grdProducts;
+        private System.Windows.Forms.GroupBox grbBill;
+        private System.Windows.Forms.DataGridView grdBill;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnCheckOut;
     }
 }
